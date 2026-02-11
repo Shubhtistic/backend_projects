e8a5c7116b89cc67d38f5c59d7e7c66533e29c9e
Shubham <theshubham1412@gmail.com>
2026-02-11 16:29:39 +0530

    Add: added ShipmentStatus enum to only allow valid Shipment Status

--- fastship/app/database/db_models.py
+++ fastship/app/database/db_models.py
@@ -1,13 +1,20 @@
-from sqlmodel import SQLModel,Field
+from enum import Enum
+from sqlmodel import SQLModel, Field
 from datetime import datetime
 from uuid import uuid4, UUID
 
-class Shipment(SQLModel,table=True):
-    __tablename__="shipment"
+
+class ShipmentStatus(str, Enum):  # using str, enum makes it behave like enum
+    Pending = "Pending"
+    InTransit = "InTransit"
+    Delivered = "Delivered"
 
 
-    id:UUID=Field(default_factory=uuid4)
-    content:str=Field(max_length=50)
-    weight:float
-    destination:int
-    estimated_delivery:datetime
+class Shipment(SQLModel, table=True):
+    __tablename__ = "shipment"
+
+    id: UUID = Field(default_factory=uuid4)
+    content: str = Field(max_length=50)
+    weight: float
+    destination: int
+    estimated_delivery: datetime
