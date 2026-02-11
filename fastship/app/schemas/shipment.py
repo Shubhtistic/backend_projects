3380973dfdd4e5d5c7e44d0c5dfc6cbd7c3a27be
Shubham <theshubham1412@gmail.com>
2026-02-11 17:09:27 +0530

    Feat: Implement shipment pydantic model for auto validation

---
+++ fastship/app/schemas/shipment.py
@@ -0,0 +1,18 @@
+from app.database.db_models import ShipmentStatus, Field, datetime
+from pydantic import BaseModel
+
+
+class BaseShipment(BaseModel):
+    content: str
+    weight: float = Field(le=25)
+    destination: int
+
+
+class ReadShipment(BaseShipment):
+    id: int
+    status: ShipmentStatus
+    estimated_delivery: datetime
+
+
+class CreateShipment(BaseShipment):
+    pass
