48c2fb152c9e80e6b9a952297e0ceb3bcd329832
Shubham <theshubham1412@gmail.com>
2026-02-15 16:15:07 +0530

    Feat: Implement auth endpoints -> login and register

--- fastship/app/api_routes/auth.py
+++ fastship/app/api_routes/auth.py
@@ -1,12 +1,19 @@
+from datetime import datetime, timedelta, timezone
 from fastapi.security import OAuth2PasswordRequestForm
 from fastapi import APIRouter, Depends
 from fastapi import status, HTTPException
 
 from app.dependancies.db_dependancy import DbSessionDep
+from app.dependancies.auth import CurrentUserDep
 from app.schemas.user_auth import CreateUser, ReadUser
 from sqlalchemy.future import select
 from app.database.db_models import Seller
 from app.security.utils import verify_hash_password, generate_hash
+from app.security.jwt import create_token
+
+from app.config import jwt_settings
+
+from app.dependancies.auth import CurrentUserDep
 
 router = APIRouter(prefix="/Auth")
 
@@ -15,7 +22,8 @@ router = APIRouter(prefix="/Auth")
 async def register_user(user: CreateUser, db: DbSessionDep):
     ## check if email already exists
 
-    qry = select(Seller).where(Seller.email == user.email)
+    normalized_email = user.email.lower()
+    qry = select(Seller).where(Seller.email == normalized_email)
 
     res = await db.execute(qry)
     existing_seller = res.scalar_one_or_none()
@@ -29,11 +37,42 @@ async def register_user(user: CreateUser, db: DbSessionDep):
     # new_user=user.model_dump()
     # ## convert pydantic schema to db
 
+    ## make email lowercase to avoid any issue
     new_seller = Seller(
-        name=user.name, email=user.email, hashed_password=hashed_password
+        name=user.name, email=normalized_email, hashed_password=hashed_password
     )
     db.add(new_seller)
     await db.commit()
     await db.refresh(new_seller)
 
     return new_seller
+
+
+@router.post("/login")
+async def lgin(
+    db: DbSessionDep,
+    form_data: OAuth2PasswordRequestForm = Depends(),
+):
+
+    normalized_email = form_data.username.lower()
+    query = select(Seller).where(Seller.email == normalized_email)
+    res = (await db.execute(query)).scalar_one_or_none()
+    if (res is None) or not verify_hash_password(
+        form_data.password, hashed_password=res.hashed_password
+    ):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Either email or Password is Wrong",
+        )
+    data = {
+        "sub": res.email,
+        "exp": datetime.now(timezone.utc)
+        + timedelta(minutes=jwt_settings.JWT_ACCESS_TOKEN_EXPIRE_MINUTES),
+    }
+    token = create_token(data=data)
+    return {"access_token": token, "token_type": "bearer"}
+
+
+@router.get("/me", response_model=ReadUser)
+async def current_user(user: CurrentUserDep):
+    return user
