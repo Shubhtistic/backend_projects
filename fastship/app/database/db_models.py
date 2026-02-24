da5f7ad70b1378c7f8f0ca5df30e79322fec3c4e
Shubham <theshubham1412@gmail.com>
2026-02-24 14:26:10 +0530

    Feat: Added Account table model now seller and shipment table rely on acccount table and refresh token table also relies on acccout table

--- fastship/app/database/db_models.py
+++ fastship/app/database/db_models.py
@@ -1,5 +1,5 @@
 from enum import Enum
-from sqlmodel import SQLModel, Field, Column, DateTime
+from sqlmodel import SQLModel, Field, Column, DateTime, Relationship, ARRAY, String
 from datetime import datetime, timezone, timedelta
 from uuid import uuid4, UUID
 from pydantic import EmailStr
@@ -9,6 +9,40 @@ class ShipmentStatus(str, Enum):  # using str, enum makes it behave like enum
     Pending = "Pending"
     InTransit = "InTransit"
     Delivered = "Delivered"
+
+
+class Account(SQLModel, table=True):
+    id: UUID = Field(default_factory=uuid4, primary_key=True)
+    name: str = Field(max_length=50)
+    email: EmailStr
+    hashed_password: str
+
+    seller: "Seller" = Relationship(back_populates="account")
+    delivery_partner: "DeliveryPartener" = Relationship(back_populates="account")
+    refresh_tokens: list["RefreshToken"] = Relationship(
+        back_populates="account", sa_relationship_kwargs={"cascade": "all, delete"}
+    )
+
+
+class Seller(SQLModel, table=True):
+    seller_id: UUID = Field(foreign_key="account.id", primary_key=True)
+
+    account: Account = Relationship(back_populates="seller")
+
+    shipment: list["Shipment"] = Relationship(back_populates="seller")
+
+
+class DeliveryPartener(SQLModel, table=True):
+    __tablename__ = "delivery_partner"
+
+    id: UUID = Field(foreign_key="account.id", primary_key=True)
+
+    account: Account = Relationship(back_populates="delivery_partner")
+
+    deliverable_zip_codes: list[str] = Field(sa_column=Column(ARRAY(String)))
+    max_handling_capacity: int
+
+    shipment: list["Shipment"] = Relationship(back_populates="delivery_partner")
 
 
 class Shipment(SQLModel, table=True):
@@ -27,12 +61,12 @@ class Shipment(SQLModel, table=True):
     # # sa_column tells sqlmodel to use a specific SQLAlchemy column type
     # DateTime(timezone=True) creates a 'TIMESTAMPTZ' column in Postgres
 
+    seller_id: UUID = Field(foreign_key="seller.seller_id")
+    delivery_partner_id: UUID = Field(foreign_key="delivery_partner.id")
 
-class Seller(SQLModel, table=True):
-    id: UUID = Field(default_factory=uuid4, primary_key=True)
-    name: str
-    email: EmailStr = Field(unique=True)
-    hashed_password: str
+    # relationship
+    seller: "Seller" = Relationship(back_populates="shipment")
+    delivery_partner: "DeliveryPartener" = Relationship(back_populates="shipment")
 
 
 class RefreshToken(SQLModel, table=True):
@@ -40,7 +74,7 @@ class RefreshToken(SQLModel, table=True):
 
     id: UUID = Field(default_factory=uuid4, primary_key=True)
 
-    user_id: UUID = Field(foreign_key="seller.id", index=True)
+    account_id: UUID = Field(foreign_key="account.id", index=True)
 
     token_hash: str
 
@@ -52,3 +86,5 @@ class RefreshToken(SQLModel, table=True):
     )
 
     revoked: bool = Field(default=False)
+
+    account: Account = Relationship(back_populates="refresh_tokens")
