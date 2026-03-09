23dd47c24fe07337406fc805b1bef2d0ac758765
Shubham <theshubham1412@gmail.com>
2026-03-09 18:07:43 +0530

    Chore(db): New alembic migration version added cascade on delete to delete all refresh tokens when user is deleted

--- seatlock/app/database/db_models.py
+++ seatlock/app/database/db_models.py
@@ -1,7 +1,7 @@
 from datetime import datetime, timezone
 from uuid import UUID
 from sqlmodel import SQLModel, Field, Column, Relationship
-from sqlalchemy import DateTime
+from sqlalchemy import DateTime, ForeignKey
 from uuid_utils import uuid7
 
 
@@ -12,7 +12,7 @@ class Users(SQLModel, table=True):
     first_name: str = Field(nullable=False, max_length=30)
     last_name: str = Field(nullable=False, max_length=30)
     email: str = Field(unique=True, index=True)
-    hashed_password: str = Field(max_length=60)
+    hashed_password: str
     mobile_no: str = Field(unique=True, max_length=15)
     is_active: bool = Field(default=True)
     created_at: datetime = Field(
@@ -25,14 +25,20 @@ class Users(SQLModel, table=True):
             DateTime(timezone=True), onupdate=lambda: datetime.now(timezone.utc)
         ),
     )
-    refresh_tokens: list["RefreshToken"] = Relationship(back_populates="user")
+    refresh_tokens: list["RefreshToken"] = Relationship(
+        back_populates="user", sa_relationship_kwargs={"cascade": "all, delete-orphan"}
+    )
 
 
 class RefreshToken(SQLModel, table=True):
     __tablename__ = "refresh_tokens"
 
     id: UUID = Field(default_factory=uuid7, primary_key=True)
-    user_id: UUID = Field(foreign_key="users.user_id", index=True)
+    user_id: UUID = Field(
+        sa_column=Column(
+            ForeignKey("users.user_id", ondelete="CASCADE"), index=True, nullable=False
+        )
+    )
     hashed_token: str = Field(index=True)
     created_at: datetime = Field(
         default_factory=lambda: datetime.now(timezone.utc),
