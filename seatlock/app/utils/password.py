364a199611b2a55e4d70faa12a023d937b97f09a
Shubham <theshubham1412@gmail.com>
2026-03-09 18:13:22 +0530

    Big Refactor(Auth): Replaced passlib/bcrypt with argon2-cffi Chnages: Removed passlib dependancy switched from bcrypt to argon2 for hashing argon2 is memory hard unlike bcrypt, defeats gpu attacks reduced login latency significantly

--- seatlock/app/utils/password.py
+++ seatlock/app/utils/password.py
@@ -1,12 +1,19 @@
-from passlib.context import CryptContext
+from argon2 import PasswordHasher
 
-password_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+ph = PasswordHasher(
+    time_cost=2,  # number of iterations or how many times the algo repeats
+    memory_cost=65536,  # 64MB RAM required per hash
+    parallelism=2,  # threads
+)
 
 
 def hash_password(raw_password) -> str:
-    """Uses bcrypt to hash the password"""
-    return password_context.hash(raw_password)
+    """Uses Argon2 to hash the password"""
+    return ph.hash(raw_password)
 
 
 def verify_hashed_password(raw_password, hashed_password) -> bool:
-    return password_context.verify(raw_password, hashed_password)
+    try:
+        return ph.verify(hashed_password, raw_password)
+    except:
+        return False
